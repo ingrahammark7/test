74598d3a4c3397abc1923f5997ccbd0adbf884e0
ingrahammark7 <ingrahammark7@gmail.com>
2022-05-23 18:58:06 -0700

    Add files via upload

--- tools/4chancaptcha/src/main/java/tools/chancaptcha/fc/fcbot.java
+++ tools/4chancaptcha/src/main/java/tools/chancaptcha/fc/fcbot.java
@@ -2,6 +2,8 @@ package tools.chancaptcha.fc;
 
 import java.awt.Robot;
 import java.awt.event.KeyEvent;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 import tools.chancaptcha.fc.util.fcutil;
 
@@ -13,8 +15,46 @@ public class fcbot {
 	public static int playy = 389;
 
 	public static void dofc() throws Exception {
-
 		Robot bot = new Robot();
+		checkrunning(bot);
+		while (true) {
+			checkrunning(bot);
+			Thread.sleep(60000L);
+			dosave(bot);
+		}
+	}
+
+	public static void checkrunning(Robot bot) throws Exception {
+		String pdinfo = "";
+		Runtime rt = Runtime.getRuntime();
+		Process p = rt.exec("tasklist");
+		BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()));
+		String line = "";
+		while ((line = input.readLine()) != null) {
+			pdinfo += line;
+		}
+		boolean isrunning = false;
+		if (pdinfo.contains("Fleet"))
+			isrunning = true;
+		if (!isrunning)
+			open(bot);
+		System.out.println("status " + isrunning);
+	}
+
+	public static void dosave(Robot bot) throws Exception {
+		int savex = 263;
+		int savey = 79;
+		fcutil.dopress(bot, KeyEvent.VK_G);
+		Thread.sleep(1000L);
+		fcutil.cli(bot, savex, savey);
+		Thread.sleep(1000L);
+		fcutil.enter(bot);
+		Thread.sleep(1000L);
+		fcutil.enter(bot);
+		Thread.sleep(1000L);
+	}
+
+	public static void open(Robot bot) throws Exception {
 		fcutil.dolocationclick(bot, locationx, locationy);
 		fcutil.enter(bot);
 		fcutil.dolocationclick(bot, playx, playy);
@@ -32,12 +72,16 @@ public class fcbot {
 		fcutil.enter(bot);
 		fcutil.enter(bot);
 		Thread.sleep(10000L);
-	}
-
-	public static void open() throws Exception {
-		while (true) {
-			fcutil.getPos();
-			Thread.sleep(1000L);
-		}
+		fcutil.dopress(bot, KeyEvent.VK_P);
+		Thread.sleep(1000L);
+		fcutil.dopress(bot, KeyEvent.VK_T);
+		Thread.sleep(1000L);
+		fcutil.dopress(bot, KeyEvent.VK_T);
+		Thread.sleep(1000L);
+		fcutil.dopress(bot, KeyEvent.VK_T);
+		Thread.sleep(1000L);
+		fcutil.dopress(bot, KeyEvent.VK_P);
+		Thread.sleep(1000L);
+		dosave(bot);
 	}
 }
