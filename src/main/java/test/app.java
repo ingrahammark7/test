26329c8ab5cfc0a1954783b1367cf27cfe715e4f
ingrahammark7 <ingrahammark7@gmail.com>
2023-08-20 02:44:11 -0700

    test

--- src/main/java/test/app.java
+++ src/main/java/test/app.java
@@ -1,9 +1,24 @@
 package test;
 
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+
 public class app {
-	
-	public static void main(String[] args) {
-		System.out.println("foo");
-	}
+
+
+
+  public static void main(String[] args) throws Exception {
+    try {
+      Robot robot = new Robot();
+
+      while (true) {
+        Thread.sleep(5000);
+        robot.keyPress(KeyEvent.VK_END);
+      }
+    } catch (AWTException e) {
+      e.printStackTrace();
+    }
+  }
 
 }
