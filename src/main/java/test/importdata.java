dff99fef89f587e4cf78f92347a5bc237b9f588b
ingrahammark7 <ingrahammark7@gmail.com>
2023-09-24 14:54:18 -0700

    Update importdata.java

--- src/main/java/test/importdata.java
+++ src/main/java/test/importdata.java
@@ -5,8 +5,6 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileInputStream;
 import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Set;
 import javax.imageio.ImageIO;
 import javax.imageio.stream.MemoryCacheImageInputStream;
 
@@ -14,23 +12,40 @@ public class importdata {
 
   // basrah is 30 n 47 e
 
+  int maxcolor = 765;
   public static double maxx = 0;
   public static double maxy = 0;
+  public static HashMap<Integer, Integer> ff = new HashMap<Integer, Integer>();
 
   public static void importing() throws Exception {
     File f = new File("img.jpg");
-    HashMap<Integer, Integer> map = load(f);
-    readMap(map);
+    load(f);
+    System.out.println(getcolor(100, 100));
   }
 
-  static void readMap(HashMap<Integer, Integer> map) {
-    System.out.println("size is " + map.size());
-    Set<Integer> key1 = map.keySet();
-    Iterator<Integer> key1it = key1.iterator();
-    Integer temp = 0;
-    while (key1it.hasNext()) {
-      temp = key1it.next();
-    }
+  public static int getcolor(int x, int y) {
+    x = getnice(x, y);
+    return sumcolor(x);
+  }
+
+  public static int sumcolor(int in) {
+    String s = Integer.toBinaryString(in);
+    System.out.println(s);
+    s = s.substring(8); // alpha removed
+    String red = s.substring(0, 8);
+    String green = s.substring(8, 16);
+    String blue = s.substring(16, 24);
+    int redi = Integer.parseInt(red, 2);
+    int greeni = Integer.parseInt(green, 2);
+    int bluei = Integer.parseInt(blue, 2);
+    redi = redi + greeni + bluei;
+    return redi;
+  }
+
+  public static int getnice(int x, int y) {
+    int realx = x * (int) maxx;
+    x = realx + y;
+    return ff.get(x);
   }
 
   static HashMap<Integer, Integer> load(File f) throws Exception {
@@ -40,7 +55,6 @@ public class importdata {
     Rectangle r = bufferedImage.getRaster().getBounds();
     maxx = r.getMaxX();
     maxy = r.getMaxY();
-    HashMap<Integer, Integer> ff = new HashMap<Integer, Integer>();
     int res = 0;
     int counter = 0;
     for (int i = 0; i < maxx / 10; ++i) {
