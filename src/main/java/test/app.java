6e2b63b0d7d532a5962bc5a9caf8468decce76c8
ingrahammark7 <ingrahammark7@gmail.com>
2023-09-24 15:09:28 -0700

    push

--- src/main/java/test/app.java
+++ src/main/java/test/app.java
@@ -5,7 +5,7 @@ public class app {
 
 
   public static void main(String[] args) throws Exception {
-    importdata.importing();
+    util.getpos();
   }
 
 
--- src/main/java/test/util.java
+++ src/main/java/test/util.java
@@ -2,25 +2,26 @@ package test;
 
 import java.awt.MouseInfo;
 import java.awt.Point;
+import java.awt.Robot;
 
 public class util {
 
   public static int wx = 150;
   public static int wy = 52;
-  public static int flattenwx = 6;// top left corner of height window
-  public static int flattenwy = 715;
+  public static int flattenwx = 5;// top left corner of height window
+  public static int flattenwy = 35;
   public static int flx = flattenwx + 21;
   public static int fly = flattenwy + 89;
   public static int frx = flx + 65;
   public static int fry = fly;
   public static int heightx = flx + 143;
   public static int heighty = fly - 2;
-  public static int ewx = 3;
-  public static int ewy = 48;
-  public static int rscrollx = ewx + 1711;
-  public static int rscrolly = ewy - 11;
-  public static int lscrollx = ewx + 1694;
-  public static int lscrolly = ewy + 977;
+  public static int ewx = 0;
+  public static int ewy = 0;
+  public static int rscrollx = ewx + 1714;
+  public static int rscrolly = ewy + 39;
+  public static int lscrollx = ewx + 1697;
+  public static int lscrolly = ewy + 1026;
 
   public static void getpos() throws Exception {
     while (true) {
@@ -32,6 +33,14 @@ public class util {
     }
   }
 
+  public static void setel(int el) throws Exception {
+    Robot r = new Robot();
 
+  }
 
+  public static void click(Robot r) {
+
+  }
 }
+
+
