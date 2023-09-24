10d55530f9f469ace12cba590bb49ceb3dc1b333
ingrahammark7 <ingrahammark7@gmail.com>
2023-09-24 12:04:28 -0700

    push

--- src/main/java/test/importdata.java
+++ src/main/java/test/importdata.java
@@ -24,22 +24,12 @@ public class importdata {
   }
 
   static void readMap(HashMap<Integer, Integer> map) {
-
+    System.out.println("size is " + map.size());
     Set<Integer> key1 = map.keySet();
     Iterator<Integer> key1it = key1.iterator();
-    HashMap<Integer, Integer> working = new HashMap<Integer, Integer>();
-    int temp = 0;
-    int x = 0;
+    Integer temp = 0;
     while (key1it.hasNext()) {
       temp = key1it.next();
-      x = temp;
-      System.out.println("x is " + temp);
-      Set<Integer> set2 = working.keySet();
-      System.out.println("size " + set2.size());
-      while (working.keySet().iterator().hasNext()) {
-        temp = working.keySet().iterator().next();
-        System.out.println("working " + x + " " + temp);
-      }
     }
   }
 
@@ -52,23 +42,12 @@ public class importdata {
     maxy = r.getMaxY();
     HashMap<Integer, Integer> ff = new HashMap<Integer, Integer>();
     int res = 0;
-    int keyx = 0;
-    int maxintx = (int) maxx;
-    int maxinty = (int) maxy;
-    int[] xkeys = new int[maxintx * maxinty];
-    for (int i = 0; i < maxintx; ++i) {
-      for (int k = 0; k < maxinty; ++k) {
-        int foo = i * maxintx;
-        xkeys[i] = foo;
-      }
-    }
-    for (int i = 0; i < maxx; ++i) {
-      if (i % 100 == 0)
-        System.out.println("progress: " + i / maxx);
+    int counter = 0;
+    for (int i = 0; i < maxx / 10; ++i) {
       for (int k = 0; k < maxy; ++k) {
-        keyx = xkeys[i] + k;
         res = bufferedImage.getRGB(i, k);
-        ff.put(keyx, res);
+        ff.put(counter, res);
+        ++counter;
       }
     }
     return ff;
