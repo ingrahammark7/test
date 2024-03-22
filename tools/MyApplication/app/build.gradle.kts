70e9763bf3f92865489e899adc634268ac8f7140
ingrahammark7 <ingrahammark7@gmail.com>
2024-03-21 23:14:52 -0700

    test

--- tools/MyApplication/app/build.gradle.kts
+++ tools/MyApplication/app/build.gradle.kts
@@ -56,6 +56,8 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.media3:media3-common:1.3.0")
+    implementation("androidx.compose.material3:material3-android:1.2.1")
+    implementation("androidx.datastore:datastore-core-android:1.1.0-beta02")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
