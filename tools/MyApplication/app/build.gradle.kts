dd2d67aaee98fa50e861d44b4b002f7692d62001
ingrahammark7 <ingrahammark7@gmail.com>
2024-03-22 21:23:46 -0700

    t

--- tools/MyApplication/app/build.gradle.kts
+++ tools/MyApplication/app/build.gradle.kts
@@ -58,6 +58,7 @@ dependencies {
     implementation("androidx.media3:media3-common:1.3.0")
     implementation("androidx.compose.material3:material3-android:1.2.1")
     implementation("androidx.datastore:datastore-core-android:1.1.0-beta02")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- tools/MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ tools/MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,14 +1,117 @@
 package com.example.myapplication
+
+import android.R.attr.action
+import android.R.id
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.os.StrictMode
+import android.os.StrictMode.ThreadPolicy
+import android.view.View
+import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.Text
+import androidx.compose.ui.input.key.Key.Companion.Calendar
+import java.io.BufferedReader
+import java.net.HttpURLConnection
+import java.net.URL
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        printDifferenceDateForHours()
         setContent {
-            Text("Hello world!")
+            val policy = ThreadPolicy.Builder().permitAll().build()
+            StrictMode.setThreadPolicy(policy)
+            var foo = sendGet("https://www.google.com/")
+            Text(foo)
+            foo = sendGet("https://www.facebook.com/")
+            Text(foo)
         }
     }
+}
+
+
+private var Int.text: String
+    get() {
+        return "0"
+    }
+    set(foo: String){
+
+     }
+private lateinit var countDownTimer: CountDownTimer
+
+fun printDifferenceDateForHours() {
+
+    val currentTime = java.util.Calendar.getInstance().time
+    val endDateDay = "03/02/2020 21:00:00"
+    val format1 = SimpleDateFormat("dd/MM/yyyy hh:mm:ss", Locale.getDefault())
+    val endDate = format1.parse(endDateDay)
+
+    val txt_timeleft = 0
+    //milliseconds
+    var different = endDate.time - currentTime.time
+    countDownTimer = object : CountDownTimer(different, 1000) {
+
+        override fun onTick(millisUntilFinished: Long) {
+            var diff = millisUntilFinished
+            val secondsInMilli: Long = 1000
+            val minutesInMilli = secondsInMilli * 60
+            val hoursInMilli = minutesInMilli * 60
+            val daysInMilli = hoursInMilli * 24
+
+            val elapsedDays = diff / daysInMilli
+            diff %= daysInMilli
+
+            val elapsedHours = diff / hoursInMilli
+            diff %= hoursInMilli
+
+            val elapsedMinutes = diff / minutesInMilli
+            diff %= minutesInMilli
+
+            val elapsedSeconds = diff / secondsInMilli
+
+            txt_timeleft.text = "$elapsedDays days $elapsedHours hs $elapsedMinutes min $elapsedSeconds sec"
+        }
+
+        override fun onFinish() {
+            txt_timeleft.text = "done!"
+        }
+    }.start()
+}
+
+fun sendGet(name: String ):String {
+    val sb = StringBuilder()
+    val url = URL(name)
+    with(url.openConnection() as HttpURLConnection) {
+        requestMethod = "GET"  // optional default is GET
+
+        println("\nSent 'GET' request to URL : $url; Response Code : $responseCode")
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            inputStream.bufferedReader().use {
+                it.lines().forEach { line ->
+                    println(line)
+                    sb.append(line)
+                }
+            }
+        } else {
+            val reader: BufferedReader = inputStream.bufferedReader()
+            var line: String? = reader.readLine()
+            while (line != null) {
+                System.out.println(line)
+                line = reader.readLine()
+            }
+            reader.close()
+
+        }
+    }
+    System.gc()
+    return sb.toString()
 }
