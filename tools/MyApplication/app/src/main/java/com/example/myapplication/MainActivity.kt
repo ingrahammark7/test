737cb7ebef99a760610a77e637b66caa129d87b8
ingrahammark7 <ingrahammark7@gmail.com>
2024-03-24 15:00:56 -0700

    Update MainActivity.kt

--- tools/MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ tools/MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,27 +1,29 @@
 package com.example.myapplication
 
-import android.content.Context
-import android.graphics.Bitmap
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
+import android.app.DownloadManager
 import android.os.Build
 import android.os.Bundle
 import android.os.Environment
 import android.os.StrictMode
 import android.os.StrictMode.ThreadPolicy
 import android.util.Log
-import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import java.io.BufferedReader
+import java.io.File
+import java.io.FileInputStream
+import java.io.IOException
 import java.net.HttpURLConnection
 import java.net.URL
 import java.util.regex.Pattern
 
 
+var alreadyFile = "alreadydone.txt"
+var basepath = "https:/www.google.com/"
+var nextfile = basepath
+var pathf = File("")
+
 class MainActivity : ComponentActivity() {
-    var alreadyFile = "alreadydone.txt"
     val urlPattern: Pattern = Pattern.compile(
         "(?:^|[\\W])((ht|f)tp(s?):\\/\\/|www\\.)"
                 + "(([\\w\\-]+\\.){1,}?([\\w\\-.~]+\\/?)*"
@@ -35,15 +37,23 @@ class MainActivity : ComponentActivity() {
         val policy = ThreadPolicy.Builder().permitAll().build()
         StrictMode.setThreadPolicy(policy)
         super.onCreate(savedInstanceState)
-
         setContent {
-            var f = sendGet("https://www.google.com/")
-            val listOfUrls = getHyperLinks(f)
-            val pathf = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
-            Log.d("filepath",pathf.absolutePath)
-            //storage/emulated/0/Downloads
-            for(i in listOfUrls){
+            pathf =
+                Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+            looper()
         }
+    }
+
+    fun looper(){
+        while(true) {
+            var filesd = readfile(alreadyFile)
+            if(filesd.contains(nextfile)) continue
+            if(!nextfile.contains(basepath)) continue
+            var f = sendGet(basepath)
+            val listOfUrls = getHyperLinks(f)
+            for (i in listOfUrls) {
+            }
+            break
         }
     }
 
@@ -63,29 +73,76 @@ class MainActivity : ComponentActivity() {
 }
 
 fun alreadyVisit(name: String){
-
+    writefile(alreadyFile,name)
 }
 
 fun appendfile(name:String, content:String){
     var foo = readfile(name)
     var sb = StringBuilder()
     sb.append(foo)
+    Log.d("foof",name)
+    Log.d("foof",sb.toString())
     writefile(name,sb.toString())
 }
 
 fun readfile(name: String): String{
-    return ""
+    val file = File(pathf, name)
+    Log.d("ffer",file.toString())
+    file.appendText("foo")
+    file.createNewFile()
+    return FileInputStream(file).bufferedReader().use { it.readText() }
 }
 
-fun writefile(name:String, content:String){
+fun getoldpath(name:String): String{
+    var parts = name.split("/")
+    Log.d("startname",name)
+    var sb = StringBuilder()
+    var i = 0;
+    for(part in parts){
+        if(i == parts.size) continue
+        if(part.equals("")) continue
+        sb.append(part + "/")
+        i += 1
+    }
+    return sb.toString()
+}
 
+fun getfullfilename(name:String): String{
+    var sb = StringBuilder()
+    sb.append(pathf)
+    sb.append("/")
+    sb.append(name)
+    return sb.toString()
+}
+
+fun writefile(pp:String,name:String){
+    //name is data
+    //p is path
+    Log.d("initialname",name)
+    Log.d("pper1",pp)
+    var pp1 = getfullfilename(pp)
+    Log.d("pper2",pp1)
+    var bar = pp1.split("//")
+    var newname = pp1.split("//")[bar.size-1]
+    var foo = StringBuilder()
+    foo.append(pathf)
+    foo.append(pp1)
+    Log.d("fooer",foo.toString())
+    var oldpath = getoldpath(foo.toString())
+    Log.d("oldapth",oldpath)
+    Log.d("nane",newname)
+    val file = File(oldpath, newname)
+    Log.d("existfoo",newname)
+    Log.d("olde",oldpath)
+//    file.createNewFile()
+ //   file.appendText(pp1)
 }
 
 fun sendGet(name: String ):String {
     val sb = StringBuilder()
     val url = URL(name)
     with(url.openConnection() as HttpURLConnection) {
-        requestMethod = "GET"  // optional default is GET
+        requestMethod = "GET"
 
         println("\nSent 'GET' request to URL : $url; Response Code : $responseCode")
 
@@ -108,5 +165,6 @@ fun sendGet(name: String ):String {
         }
     }
     System.gc()
+    appendfile(alreadyFile,name+"\n")
     return sb.toString()
 }
