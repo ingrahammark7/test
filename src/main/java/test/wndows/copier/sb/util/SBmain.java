c3213c43c7f565a93264fa993d4765da3fa6627c
ingrahammark7 <ingrahammark7@gmail.com>
2024-04-08 18:56:07 -0700

    f

--- src/main/java/test/wndows/copier/sb/util/SBmain.java
+++ src/main/java/test/wndows/copier/sb/util/SBmain.java
@@ -1,31 +1,10 @@
 package test.wndows.copier.sb.util;
 
-import java.util.ArrayList;
-import test.wndows.copier.sb.util.web.fileutil;
-import test.wndows.copier.sb.util.web.webutil;
-
 public class SBmain {
 
 
   public static void dof() throws Exception {
-    try {
-      ArrayList<String> foof = new ArrayList<String>();
-      String ssf = fileutil.read("in.txt");
-      String[] ff = ssf.split("\n");
-      for (int i = 0; i < ff.length; ++i) {
-        foof.add(ff[i]);
-      }
-      for (int i = 0; i < foof.size(); ++i) {
-        String s = webutil.doreq(foof.get(i));
-        String[] ss = s.split("\"authorId\":\"");
-        s = ss[1];
-        s = s.split("\"")[0];
-        s = "https://www.pixiv.net/en/users/" + s + "\n";
-        fileutil.write(s);
-      }
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
+
   }
 
   public static void sb() throws Exception {
