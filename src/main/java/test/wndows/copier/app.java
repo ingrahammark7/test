c84b6510743fac840457354b226ac9aee7fe7550
ingrahammark7 <ingrahammark7@gmail.com>
2024-04-26 04:54:02 -0700

    f

--- src/main/java/test/wndows/copier/app.java
+++ src/main/java/test/wndows/copier/app.java
@@ -1,10 +1,11 @@
 package test.wndows.copier;
 
+import test.wndows.copier.sb.util.apputil.sbutils;
+
 public class app {
 
   public static void main(String[] args) throws Exception {
-    // sbutils.dofirst();
-    pixer.pixef();
+    sbutils.dofirst();
   }
 
 
