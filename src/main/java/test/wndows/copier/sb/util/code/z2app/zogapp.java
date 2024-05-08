010ce22e4c096e923ae737043a60427fed07358a
ingrahammark7 <ingrahammark7@gmail.com>
2024-05-08 10:40:41 -0700

    Update zogapp.java

--- src/main/java/test/wndows/copier/sb/util/code/z2app/zogapp.java
+++ src/main/java/test/wndows/copier/sb/util/code/z2app/zogapp.java
@@ -3,6 +3,8 @@ package test.wndows.copier.sb.util.code.z2app;
 import java.util.ArrayList;
 import test.wndows.copier.sb.util.web.fileutil;
 
+// gallery-dl -i C:\Users\a\Documents\GitHub\test\foo.txt --http-timeout 1000
+
 public class zogapp {
 
   public static void doer() throws Exception {
@@ -14,14 +16,18 @@ public class zogapp {
       String f2 = f1.split("/")[1];
       foff.add("https://" + f2);
     }
-    String comm = "mdkir ";
+    String comm = "mkdir ";
     String com1 = " && cd ";
     String com2 = " && nohup httrack ";
-    String comm2 = "> s.txt &";
+    String comm2 =
+        " +*.jpg -*.wiki* --near --advanced-maxlinks=10000000000000 -s0 > s.txt &; cd ..";
     for (String f : foff) {
       Thread.sleep(2);
       String ss = String.valueOf(System.currentTimeMillis());
-      System.out.println(comm + ss + com1 + ss + com2 + f + comm2);
+      String sf = comm + ss + com1 + ss + com2 + f + comm2;
+      sf = sf.replace("\r", "");
+      sf = sf.replace("  ", "");
+      System.out.println(sf);
     }
   }
 
