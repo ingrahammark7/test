14d4719baf8f14ca2f2b6b1b2904788df0013045
ingrahammark7 <ingrahammark7@gmail.com>
2024-05-09 14:24:42 -0700

    f

--- src/main/java/test/wndows/copier/app.java
+++ src/main/java/test/wndows/copier/app.java
@@ -1,11 +1,11 @@
 package test.wndows.copier;
 
-import test.wndows.copier.sb.util.util;
+import test.wndows.copier.sb.util.code.z2app.zogapp;
 
 public class app {
 
   public static void main(String[] args) throws Exception {
-    util.getpos();
+    zogapp.d2();
     // zogapp.doer();
   }
 
