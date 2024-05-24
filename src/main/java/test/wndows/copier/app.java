33e9d0362d9f2f0fa04ea919de47ad2b1faad3cb
ingrahammark7 <ingrahammark7@gmail.com>
2024-05-23 17:02:41 -0700

    Update app.java

--- src/main/java/test/wndows/copier/app.java
+++ src/main/java/test/wndows/copier/app.java
@@ -1,12 +1,13 @@
 package test.wndows.copier;
 
-import test.wndows.copier.sb.util.code.z2app.zogapp;
+import test.wndows.copier.sb.util.web.fileutil;
 
 public class app {
 
   public static void main(String[] args) throws Exception {
     // zogapp.d2();
-    zogapp.doer();
+    // zogapp.doer();
+    fileutil.d3();
   }
 
 
