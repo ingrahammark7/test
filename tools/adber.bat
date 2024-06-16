881836913ebe4589c56d7f0f8007e3f787584310
ingrahammark7 <ingrahammark7@gmail.com>
2024-06-16 07:59:40 -0700

    Update adber.bat

--- tools/adber.bat
+++ tools/adber.bat
@@ -5,7 +5,6 @@ rd /s /q e:\ge\garb\smalll5345\%FOO%\.tmp
 rd /s /q e:\ge\garb\smalll5345\%FOO%\.adblocker_1dm
 adb shell ls sdcard/Download/ > s.txt
 for /F "tokens=*" %%A in (s.txt) do ( 
-adb pull sdcard/Download/%%A e:\ge\garb\smalll5345\%TIME:~6,2%%TIME:~3,2%%TIME:~1,1%%DATE:~4,2%%DATE:~7,2%%DATE:~10,2%\ && adb shell rm -r sdcard/Download/%%A
+adb pull sdcard/Download/%%A e:\ge\garb\smalll5345\f1\%TIME:~6,2%%TIME:~3,2%%TIME:~1,1%%DATE:~4,2%%DATE:~7,2%%DATE:~10,2%\ && adb shell rm -r sdcard/Download/%%A
 )
-adb pull storage/sdcard0/Download e:\ge\garb\smalll5345\%TIME:~6,2%%TIME:~3,2%%TIME:~1,1%%DATE:~4,2%%DATE:~7,2%%DATE:~10,2%
 del s.txt
