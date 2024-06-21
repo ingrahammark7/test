c176737af2acd29956212c3641a3679310cc38ca
ingrahammark7 <ingrahammark7@gmail.com>
2024-06-21 14:47:30 -0700

    f

--- tools/dumeradsf.bat
+++ tools/dumeradsf.bat
@@ -1,4 +1,4 @@
-REM set ANDROID_SERIAL=R5CTB2C1LJA
+set ANDROID_SERIAL=R5CTB2C1LJA
 adb shell rm -f -rR -v storage/sdcard0/Download/*
 adb shell rm -f -rR -v storage/sdcard0/DCIM/Screenshots/*
 
--- tools/script.sh
+++ tools/script.sh
@@ -3,7 +3,7 @@ rm todo.txt
 rm lsof.txt
 rm lis.txt
 lsof>lsof.txt
-ls . -I libgen.sh -I storage -I script.sh | head -400  >lis.txt
+ls . -I storage -I script.sh  >lis.txt
 export fofo='lsof.txt'
 filename='lis.txt'
 for foo in $(cat lis.txt); do
