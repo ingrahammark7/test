7683dd38ea4e1e8be378bbc8343896a2e616e566
ingrahammark7 <ingrahammark7@gmail.com>
2024-07-05 04:11:44 -0700

    Update adber.bat

--- tools/adber.bat
+++ tools/adber.bat
@@ -1,10 +1,16 @@
 set ANDROID_SERIAL=R5CTB2C1LJA
 set FOO=%TIME:~6,2%%TIME:~3,2%%TIME:~1,1%%DATE:~4,2%%DATE:~7,2%%DATE:~10,2%
+mkdir e:\ge\garb\smalll5345\%FOO%
+del s.txt
+adb shell ls sdcard/DCIM/Screensohts > s.txt
+for /F "tokens=*" %%A in (s.txt) do (
+adb pull sdcard/DCIM/Screenshots/"%%A" e:\ge\garb\smalll5345\%FOO%\ && adb shell rm -r sdcard/DCIM/Screenshots/'%%A'
+)
+del s.txt
 adb pull storage/sdcard0/DCIM/Screenshots e:\ge\garb\smalll5345\%FOO%\
 rd /s /q e:\ge\garb\smalll5345\%FOO%\.tmp
 rd /s /q e:\ge\garb\smalll5345\%FOO%\.adblocker_1dm
 adb shell ls sdcard/Download/ > s.txt
-mkdir e:\ge\garb\smalll5345\%FOO%
 for /F "tokens=*" %%A in (s.txt) do ( 
 adb pull sdcard/Download/"%%A" e:\ge\garb\smalll5345\%FOO%\ && adb shell rm -r sdcard/Download/'%%A'
 )
