e510c7244d9a9c81e40c6c208891dff78d332a41
ingrahammark7 <ingrahammark7@gmail.com>
2024-07-05 04:16:07 -0700

    Update adber.bat

--- tools/adber.bat
+++ tools/adber.bat
@@ -2,7 +2,7 @@ set ANDROID_SERIAL=R5CTB2C1LJA
 set FOO=%TIME:~6,2%%TIME:~3,2%%TIME:~1,1%%DATE:~4,2%%DATE:~7,2%%DATE:~10,2%
 mkdir e:\ge\garb\smalll5345\%FOO%
 del s.txt
-adb shell ls sdcard/DCIM/Screensohts > s.txt
+adb shell ls sdcard/DCIM/Screenshots > s.txt
 for /F "tokens=*" %%A in (s.txt) do (
 adb pull sdcard/DCIM/Screenshots/"%%A" e:\ge\garb\smalll5345\%FOO%\ && adb shell rm -r sdcard/DCIM/Screenshots/'%%A'
 )
