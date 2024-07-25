71bc2bfde0418e1a2de97805da62a149c32ca374
ingrahammark7 <ingrahammark7@gmail.com>
2024-07-25 16:53:21 -0700

    Update script.sh

--- tools/drivetemplate/ge/garb/script.sh
+++ tools/drivetemplate/ge/garb/script.sh
@@ -3,7 +3,7 @@ rm todo.txt
 rm lsof.txt
 rm lis.txt
 lsof>lsof.txt
-ls . -I storage -I script.sh  >lis.txt
+ls . -I libgen.sh -I storage -I script.sh  >lis.txt
 export fofo='lsof.txt'
 filename='lis.txt'
 for foo in $(cat lis.txt); do
