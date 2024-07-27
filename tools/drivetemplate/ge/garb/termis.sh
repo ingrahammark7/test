967c0538507b0d407be8aa582cd54da46114b6d0
ingrahammark7 <ingrahammark7@gmail.com>
2024-07-27 05:59:46 -0700

    Create termis.sh

---
+++ tools/drivetemplate/ge/garb/termis.sh
@@ -0,0 +1,8 @@
+pkg upgrade -y
+pkg update -y
+termux-setup-storage
+pkg install -y libssl bash
+pkg install -y python python-pip nano ffmpeg git gcc make lsof gzip clang cmake 
+pip install yt-dlp
+pip install youtube-dl
+pip install gallery-dl
