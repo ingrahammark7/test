1f00334b40d67f7eea6a407aa9d80e6c590cb6af
ingrahammark7 <ingrahammark7@gmail.com>
2024-07-28 20:57:45 -0700

    Update pullmov.bat

--- tools/drivetemplate/ge/garb/pullmov.bat
+++ tools/drivetemplate/ge/garb/pullmov.bat
@@ -1,3 +1,14 @@
 set FOO=%TIME:~6,2%%TIME:~3,2%%TIME:~1,1%%DATE:~4,2%%DATE:~7,2%%DATE:~10,2%
+del lsof.txt
+adb shell lsof > lsof.txt
+find /c "mov1" lsof.txt >NUL
+if %errorlevel% equ 1 goto notfound
+echo found
+goto done
+:notfound
+echo notfound
 adb pull sdcard/download/mov1 e:/ge/garb/smalll5345/%FOO%/ && adb shell rm -r sdcard/download/mov1
 adb shell mkdir sdcard/download/mov1
+del lsof.txt
+goto done
+:done
