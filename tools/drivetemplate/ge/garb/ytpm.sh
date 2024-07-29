363324bf06484e487a9d641128069ac8f81159bb
ingrahammark7 <ingrahammark7@gmail.com>
2024-07-28 22:57:46 -0700

    Update ytpm.sh

--- tools/drivetemplate/ge/garb/ytpm.sh
+++ tools/drivetemplate/ge/garb/ytpm.sh
@@ -1,3 +1,5 @@
+while true;
+do (
 rm f4.txt
 cd /data/data/com.termux/files/home/
 ls *.mp4 *.m4a *.webm >f4.txt
@@ -5,3 +7,6 @@ while read foo; do
 (mv "$foo" storage/downloads/mov1/);
 done <f4.txt
 rm f4.txt
+);
+sleep 60;
+done
