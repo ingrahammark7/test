3887545c03735969d241b6cb15fdac87241a9da9
ingrahammark7 <ingrahammark7@gmail.com>
2024-07-30 03:44:25 -0700

    Update ffoo.sh

--- tools/drivetemplate/ge/garb/ffoo.sh
+++ tools/drivetemplate/ge/garb/ffoo.sh
@@ -5,7 +5,10 @@ tar czvf "$lab".tar * --remove-files
 for i in $(seq 1 1000);
 do
 (tail -c 1G "$lab".tar >"$lab"_split.part$i.tar && truncate -s -1G "$lab".tar;)
+mkdir /storage/emulated/0/Download
 mv "$lab"_split.part$i.tar ~/storage/shared/Download/
+find ~/storage/shared/Download -delete -maxdepth 1 -size 0c
+mkdir /storage/emulated/0/Download
 done
 );
 sleep 60;
