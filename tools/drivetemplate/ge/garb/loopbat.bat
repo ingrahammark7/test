c1297411469c9c66392896a94e3e7633046bd087
ingrahammark7 <ingrahammark7@gmail.com>
2024-07-30 03:56:43 -0700

    Update loopbat.bat

--- tools/drivetemplate/ge/garb/loopbat.bat
+++ tools/drivetemplate/ge/garb/loopbat.bat
@@ -6,14 +6,7 @@ mkdir e:\ge\garb\smalll5345\%FOO%
 del s.txt
 adb shell ls sdcard/Download/ > s.txt
 for /F "tokens=*" %%A in (s.txt) do ( 
-find /c %%A lsof.txt >NUL
-if %errorlevel% equ 1 goto notfound
-echo found
-goto done
-:notfound
-echo notfound
 adb pull sdcard/Download/"%%A" e:\ge\garb\smalll5345\%FOO%\ && adb shell rm -r sdcard/Download/'%%A'
-:done
 )
 del s.txt
 del lsof.txt
