c8118b9344ff1e3cc4cc07bfc247ced2e658aaa3
ingrahammark7 <ingrahammark7@gmail.com>
2024-07-30 06:55:26 -0700

    Update ffoo.sh

--- tools/drivetemplate/ge/garb/ffoo.sh
+++ tools/drivetemplate/ge/garb/ffoo.sh
@@ -6,10 +6,13 @@ for i in $(seq 1 1000);
 do
 (tail -c 1G "$lab".tar >"$lab"_split.part$i.tar && truncate -s -1G "$lab".tar;)
 mkdir /storage/emulated/0/Download
+if [ -s "$lab"_split.part$i.tar ]; then
+echo good
+else
+rm "$lab"_split.part$i.tar
+fi
 mv "$lab"_split.part$i.tar ~/storage/shared/Download/
-find ~/storage/shared/Download -delete -maxdepth 1 -size 0c
 mkdir /storage/emulated/0/Download
-find * -delete -size 0c
 done
 );
 sleep 60;
