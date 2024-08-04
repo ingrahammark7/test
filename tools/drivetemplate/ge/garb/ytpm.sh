ab55484647fbf3c1c7c4ca66b653fc2bb1edca7f
ingrahammark7 <ingrahammark7@gmail.com>
2024-08-04 09:12:44 -0700

    f

--- tools/drivetemplate/ge/garb/ytpm.sh
+++ tools/drivetemplate/ge/garb/ytpm.sh
@@ -2,7 +2,7 @@ while true;
 do (
 rm f4.txt
 cd /data/data/com.termux/files/home/
-ls *.mp4 *.m4a *.webm >f4.txt
+ls *.mp4 *.m4a *.webm *.mkv >f4.txt
 while read foo; do
 (mkdir /storage/emulated/0/Download
 mv "$foo" storage/downloads/mov1/);
