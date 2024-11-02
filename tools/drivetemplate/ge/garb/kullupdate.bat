02a07fe53370aa292fdf71051d1cd3a5f66c861d
ingrahammark7 <ingrahammark7@gmail.com>
2024-11-02 14:54:01 -0700

    Update kullupdate.bat

--- tools/drivetemplate/ge/garb/kullupdate.bat
+++ tools/drivetemplate/ge/garb/kullupdate.bat
@@ -3,3 +3,4 @@ pm list packages | grep soagent
 pm list packages | grep wssyncmldm
 pm disable-user --user 0 com.sec.android.soagent
 pm disable-user --user 0 com.wssyncmldm
+appops set com.motorola.ccc.ota RUN_IN_BACKGROUND deny
