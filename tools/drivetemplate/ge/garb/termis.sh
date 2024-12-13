a3f8f2d070cbdb48e18e73284d9a85f605b5ab74
ingrahammark7 <ingrahammark7@gmail.com>
2024-12-13 15:19:08 -0800

    Update termis.sh

--- tools/drivetemplate/ge/garb/termis.sh
+++ tools/drivetemplate/ge/garb/termis.sh
@@ -2,7 +2,7 @@ pkg upgrade -y
 pkg update -y
 termux-setup-storage
 pkg install -y libssl bash
-pkg install -y wget wget2 e2fsprogs python python-pip nano ffmpeg git make lsof gzip clang cmake 
+pkg install -y firefox geckodriver wget wget2 e2fsprogs python python-pip nano ffmpeg git make lsof gzip clang cmake 
 pip install yt-dlp
 pip install youtube-dl
 pip install gallery-dl
