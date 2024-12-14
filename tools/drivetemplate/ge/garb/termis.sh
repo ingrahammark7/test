8da653810f79d12779414674e57349e4046547ca
ingrahammark7 <ingrahammark7@gmail.com>
2024-12-14 14:02:03 -0800

    Update termis.sh

--- tools/drivetemplate/ge/garb/termis.sh
+++ tools/drivetemplate/ge/garb/termis.sh
@@ -3,10 +3,4 @@ pkg update -y
 termux-setup-storage
 pkg install -y libssl bash
 pkg install -y libxml2 firefox geckodriver wget wget2 e2fsprogs python python-pip nano ffmpeg git make lsof gzip clang cmake 
-pip install yt-dlp
-pip install setuptools
-pip install youtube-dl
-pip install gallery-dl
-pip install beautifulsoup4
-pip install build
-pip install lxml
+pip install yt-dlp setuptools youtube-dl gallery-dl beautifulsoup4 build lxml numpy
