a38a332a7e0f1597c8ac4374b9de24d6094072ff
ingrahammark7 <ingrahammark7@gmail.com>
2024-12-15 15:06:28 -0800

    Update termis.sh

--- tools/drivetemplate/ge/garb/termis.sh
+++ tools/drivetemplate/ge/garb/termis.sh
@@ -2,7 +2,6 @@ pkg upgrade -y
 pkg update -y
 termux-setup-storage
 pkg install -y libssl bash
-pkg install -y python-pip
 pkg install -y libxml2 firefox geckodriver wget wget2 e2fsprogs python python-pip nano ffmpeg git make lsof gzip clang cmake 
 pip install yt-dlp setuptools youtube-dl gallery-dl beautifulsoup4 build 
 CFLAGS="-Wno-error=incompatible-function-pointer-types -O0" pip install lxml
