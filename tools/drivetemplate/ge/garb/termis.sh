eb75774944cfe1ae88f8ead2651e6deae2a19532
ingrahammark7 <ingrahammark7@gmail.com>
2025-02-26 19:41:46 -0800

    Update termis.sh

--- tools/drivetemplate/ge/garb/termis.sh
+++ tools/drivetemplate/ge/garb/termis.sh
@@ -3,7 +3,7 @@ pkg update -y
 termux-setup-storage
 pkg install -y bash
 pkg install -y libxml2 geckodriver wget wget2 e2fsprogs python python-pip nano ffmpeg git make lsof gzip clang cmake htop bat fd ripgrep tig asciinema neofetch  nnn aria2 mc mtr  lftp fzf pacman termux-api
-pip install yt-dlp setuptools youtube-dl gallery-dl beautifulsoup4 build reportlab sympy pypdf2 poppler ghostscript
+pip install yt-dlp setuptools youtube-dl gallery-dl beautifulsoup4 build reportlab sympy pypdf2 poppler ghostscript mps-youtube
 apt install root-repo
 apt install iw
 apt install libc++ libnl libpcap libsqlite openssl pcre zlib -y
