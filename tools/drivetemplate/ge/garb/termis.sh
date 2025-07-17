1b5d243b14700958688d4f36ce70e15a7d133ecb
ingrahammark7 <ingrahammark7@gmail.com>
2025-07-16 18:18:26 -0700

    Update termis.sh

--- tools/drivetemplate/ge/garb/termis.sh
+++ tools/drivetemplate/ge/garb/termis.sh
@@ -3,7 +3,7 @@ pkg update -y
 termux-setup-storage
 pkg install -y bash
 pkg install -y emacs wget libxml2 geckodriver wget wget2 e2fsprogs python python-pip nano ffmpeg git make lsof gzip clang cmake htop bat fd ripgrep tig asciinema neofetch  nnn aria2 mc lftp fzf pacman termux-api pdftk imagemagick libffi freetype pango cmatrix sl figlet toilet cowsay fortune ninvaders moon-buggy greed rclone pv
-pip install yt-dlp setuptools youtube-dl gallery-dl beautifulsoup4 build reportlab sympy pypdf2 poppler ghostscript mps-youtube black pyautogui pynput pywinauto pygetwindow uiautomator kivy kivy3 numpy-stl
+pip install yt-dlp setuptools youtube-dl gallery-dl beautifulsoup4 build reportlab sympy pypdf2 poppler ghostscript mps-youtube black pyautogui pynput pywinauto pygetwindow uiautomator kivy kivy3 numpy-stl pyopengl pyopengl_accelerate
 apt install root-repo -y
 apt install iw -y
 apt install libc++ libnl libpcap libsqlite openssl pcre zlib -y
