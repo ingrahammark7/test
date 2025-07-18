19a96df4c3eeb6a6301d1ef039bf8fe95c0382f5
ingrahammark7 <ingrahammark7@gmail.com>
2025-07-17 18:30:31 -0700

    Add files via upload

--- tools/Misc/gw.py
+++ tools/Misc/gw.py
@@ -33,8 +33,23 @@ F_g1 = G * m_p**2 / d**2
 impulse_per_sec1 = F_g1 * dt1 * f1 # This should equal F_g
 ff=math.pow(10,38)
 fd=impulse_per_sec/impulse_per_sec1
-print(f"Ratio impulse per sec to instantaneous force: {fd:.3f}")
-m=math.log10(fd)
-print("log ratio")
-print(m)
+print(f"Ratio impulse per sec to instantaneous force: {fd:.3e}")
 #gravity is 10^44 times stronger than the strong force
+planck_mass = 2.18e-8
+c=3*math.pow(10,8)
+amu = 1.66e-27  # kg
+f=(4*math.pi)-6
+phi = 1.6180339887
+f=f**phi
+f=f**phi
+f6=f**(phi)
+f7=f6
+f6=f6**6
+f6=(f6**1.25)/f
+planck_mass = 2.18e-8
+pm=10**-12
+cr=140*pm
+rate=(c/cr)
+av=6*math.pow(10,23)
+cm=amu*24
+cper=planck_mass/cm
