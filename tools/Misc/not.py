bddd597e5e5cd8daab1b391c1aa70f68e888c975
ingrahammark7 <ingrahammark7@gmail.com>
2025-09-17 13:55:49 -0700

    Add files via upload

--- tools/Misc/not.py
+++ tools/Misc/not.py
@@ -4,7 +4,7 @@ from reportlab.lib.units import inch
 from reportlab.lib.styles import getSampleStyleSheet
 from reportlab.platypus import Paragraph, Frame
 
-footer_text = "Notice of Motion" 
+footer_text = "Notice of Appeal" 
 
 def create_pdf(output_filename):
     # Create a PDF document
@@ -109,7 +109,7 @@ def create_pdf(output_filename):
     c.setFont("Times-Bold", 12)
     c.drawString(left_margin, y_position - 0*inch, footer_text.upper())
     c.setFont("Times-Roman",12)
-    intro_text = "I called court to schedule motion to compel decision. Court ignored the request. Compel judge to make decision. /n" 
+    intro_text = "I am appealing the decision of the judge to not make a decision despite repeated requests to do so. Notice mailed to defendant at: /n 100 W 1st St /n Los Angeles CA 90012 /n" 
     y_position -= .25*inch
     
     
