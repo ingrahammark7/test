1fd941b9d2afdde250573a9d84543a815abd6bcb
ingrahammark7 <ingrahammark7@gmail.com>
2025-09-28 10:27:48 -0700

    Add files via upload

--- tools/Misc/gw1.py
+++ tools/Misc/gw1.py
@@ -1,24 +1,26 @@
-from mpmath import mp, power, pi, sqrt
+import math
 
-# Set maximum precision
-mp.dps = 100
-
-# Constants
-f = mp.mpf('1.45e-6')             # fraction of water permittivity needed
-mass_water = mp.mpf('2.992e-26')  # kg, one water molecule
-density_water = mp.mpf('1000')    # kg/m^3
-
-# Mass ratio
-mass_object = mass_water / f
-print(f"Mass of object: {mass_object} kg")
-
-# Atom count (approximate, assuming 1 atom per water mass unit)
-atom_count = mass_object / mass_water
-print(f"Approximate number of water-molecule masses: {atom_count}")
-
-# Volume of object
-volume_object = mass_object / density_water  # m^3
-
-# Radius of object assuming spherical
-radius_object = (3 * volume_object / (4 * pi))**(1/3)
-print(f"Radius of object: {radius_object} m")
+startuse=600_000
+pct=.002
+tot=startuse/pct
+pc1=pct
+months=12
+rate=1.08
+clock=6
+peruse=.5
+pb=peruse
+de=0
+for i in range(72):
+	print("year ",(i/12)+2025)
+	pct*=rate
+	peruse=(pb**clock)+pb+pct
+	if(peruse>1):
+		peruse=1
+	f=peruse**clock
+	print(f)
+	det=pct*f*tot
+	de+=det
+	print(de)
+	
+	
+	
