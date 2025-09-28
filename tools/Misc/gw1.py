91683cd4e42fa1d1f80fb19701eccf92fdc1f1a7
ingrahammark7 <ingrahammark7@gmail.com>
2025-09-28 10:36:59 -0700

    Add files via upload

--- tools/Misc/gw1.py
+++ tools/Misc/gw1.py
@@ -7,20 +7,20 @@ pc1=pct
 months=12
 rate=1.08
 clock=6
-peruse=.5
+peruse=(1/3)
 pb=peruse
 de=0
-for i in range(72):
+for i in range(84):
 	print("year ",(i/12)+2025)
 	pct*=rate
-	peruse=(pb**clock)+pb+pct
+	peruse=pb+(pct**clock)
 	if(peruse>1):
 		peruse=1
 	f=peruse**clock
 	print(f)
 	det=pct*f*tot
 	de+=det
-	print(de)
+	print(de/1_000,"k")
 	
 	
 	
