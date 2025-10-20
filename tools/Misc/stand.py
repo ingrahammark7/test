cf86381177f1ad61c03f8f86dace5e2c3ad491d7
ingrahammark7 <ingrahammark7@gmail.com>
2025-10-19 19:36:50 -0700

    Add files via upload

--- tools/Misc/stand.py
+++ tools/Misc/stand.py
@@ -4,7 +4,7 @@ from reportlab.lib.units import inch
 from reportlab.lib.styles import getSampleStyleSheet
 from reportlab.platypus import Paragraph, Frame
 
-footer_text = "Request for Order" 
+footer_text = "Request for Order"
 
 def create_pdf(output_filename):
     # Create a PDF document
@@ -22,7 +22,7 @@ def create_pdf(output_filename):
 
     # Add page numbers (starting from page 2)
     def add_page_number(page_num):
-        if page_num > 1:  # Suppress page number on the first page
+        if page_num > 2:  # Suppress page number on the first page
             c.setFont("Times-Roman", 10)
             c.drawCentredString(width / 2, 0.5 * inch, f"{page_num}")
 
@@ -45,12 +45,30 @@ def create_pdf(output_filename):
             line_number += 1
             current_y -= line_height
 
-   
+    # First Page Format (Rule 2.111)
+    # (1) Attorney Information
+    c.setFont("Times-Roman", 12)
+    attorney_info = [
+        "Mark Ingraham",
+        "3553 Atlantic Avenue",
+        "Long Beach, CA 90807",
+        "Telephone: (408) 660-5425",
+        "Email: ingrahammark7@gmail.com"
+    ]
+    def doy():
+    	return top_margin - 0.5 * inch
+    y_position = doy()
+    for line in attorney_info:
+        c.drawString(left_margin, y_position, line)
+        y_position -= 0.25 * inch
+
+    # (2) Clerk's Space (blank space to the right of center)
+    c.rect(width / 2 + 0.5 * inch, top_margin - 2 * inch, 2 * inch, 1.5 * inch, stroke=1, fill=0)
 
     # (3) Court Title
     c.setFont("Times-Bold", 14)
-    c.drawCentredString(width / 2, top_margin - 0 * inch, "UNITED STATES COURT OF APPEALS")
-    c.drawCentredString(width / 2, top_margin - 0.33 * inch, "NINTH CIRCUIT")
+    c.drawCentredString(width / 2, top_margin - 3.33 * inch, "CALIFORNIA COURT OF APPEALS")
+    c.drawCentredString(width / 2, top_margin - 3.66 * inch, "SECOND DISTRICT")
  
 
     # (4) Case Title
@@ -59,48 +77,44 @@ def create_pdf(output_filename):
         "Mark Ingraham,",
         "Plaintiff and Petitioner,",
         "vs",
-        "Los Angeles Police Department",
-        "Respondent and Defendant.",        
+        "Los Angeles Superior Court,",
+        "Respondent,",
+        "Los Angeles Police Department,",
+        "Real Party In Interest.",        
     ]
-    y_position = top_margin - 1.75* inch
+    y_position = top_margin - 4.75* inch
     counter=0
     for line in case_title:
         c.setFont("Times-Bold",12)
-        if counter==1 or counter==4:
+        if counter==1 or counter==4 or counter==6:
         	c.setFont("Times-Roman",12)
-        c.drawCentredString(width/2, y_position, line)
+        c.drawString(left_margin, y_position, line)
         y_position -= 0.25 * inch
         counter+=1
 
-    # (5) Case Numbe
-    c.setFont("Times-Roman", 12)
+    # (5) Case Number
+    c.setFont("Times-Bold", 12)
     space=width / 2 + 1 * inch
-    
-    y_position=top_margin-4*inch
-    
-    # First Page Format (Rule 2.111)
-    # (1) Attorney Information
+    c.drawString(space, top_margin - 4* inch, "Superior Court Case Number: 25STCP00742")
+    # (6) Nature of the Paper (moved slightly lower)
+    c.setFont("Times-Bold", 12)
+    c.drawString(space, top_margin - 4.25* inch, "Appellate Case Number: B349749")
+    c.setFont("Times-Bold", 12)
+    c.drawString(space, top_margin - 4.5* inch, "")
+
+    # (7) Judge and Department
     c.setFont("Times-Roman", 12)
-    attorney_info = [
-        "Case Number: 25-6407",
-        "Mark Ingraham",
-        "3553 Atlantic Avenue",
-        "Long Beach, CA 90807",
-        "Telephone: (408) 660-5425",
-        "Email: ingrahammark7@gmail.com"
-    ]
-    
-    for line in attorney_info:
-        c.drawString(space, y_position, line)
-        y_position -= 0.25 * inch 
+    c.drawString(space, top_margin - 4.75 * inch, "Judge: ")
+    c.drawString(space, top_margin - 5* inch, "Department: ")
+
    
 
 
     # I. INTRODUCTION
     c.setFont("Times-Bold", 12)
-    c.drawCentredString(width/2, top_margin -0.75* inch, footer_text.upper())
+    c.drawString(left_margin, y_position - 0.25* inch, footer_text.upper())
     c.setFont("Times-Roman",12)
-    intro_text = "/n" 
+    intro_text = "All orders I requested on my previous cases apply to all my cases. /n Previous orders attached. /n" 
     y_position -= .25*inch
     
     
@@ -124,48 +138,34 @@ def create_pdf(output_filename):
     		tt+=s
     	for line in intro_text1:
     		c.drawString(left_margin, y_position, line)
-    		y_position -= 0.5* inch
+    		y_position -= 0.25* inch
     	return y_position
     	
-    y_position=doer(intro_text,y_position)
-    
-    
+       
     
     
     def dopage(cc,num):
     	# Add line numbers to the first page
     	line_height = 0.25 * inch  # Adjusted to match text spacing
-    	if(num>1):
-    		add_line_numbers(top_margin - 0.5 * inch, bottom_margin + 0.5 * inch, line_height)
+    	add_line_numbers(top_margin - 0.5 * inch, bottom_margin + 0.5 * inch, line_height)
     	add_footer(num)
     	cc.showPage()
     	return cc
     	
-   
+    
     c=dopage(c,1)
-    
-    def heads(text,y_position):
-    	y_position-=.25*inch
-    	c.setFont("Times-Bold", 12)
-    	c.drawString(left_margin, y_position, text)
-    	c.setFont("Times-Roman",12)
-    	intro_text = "/n" 
-    	y_position -= .25*inch
-    	return y_position
-    	
-    
-    h1=""
-    y_position=top_margin-.5*inch
-    y_position=heads(h1,y_position)    
-    
+    y_position=top_margin-.25*inch
+    c.setFont("Times-Bold", 12)
+    c.drawString(left_margin, y_position - 0.25* inch, footer_text.upper())
+    y_position-=.5*inch
+    y_position=doer(intro_text,y_position)
     c.setFont("Times-Roman", 12)
-    text="All orders I requested on my previous cases apply to all my cases. /n Previous orders attached. /n"
-    y_position=doer(text,y_position)
-    c.drawString(left_margin, y_position - 0.25* inch, "Submitted, Mark Ingraham, 10/4/2025")
+    c.drawString(left_margin, y_position - 0.25* inch, "Submitted, Mark Ingraham, 9/19/2025")
+    c=dopage(c,2)
+ 
 
 
     # Save the PDF
-    c=dopage(c,2)
     c.save()
 
 if __name__ == "__main__":
