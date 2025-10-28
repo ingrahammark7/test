f25ae671a3eaef52ab73861abe4fad053749c4e4
ingrahammark7 <ingrahammark7@gmail.com>
2025-10-28 14:11:57 -0700

    Add files via upload

--- tools/Misc/pen.py
+++ tools/Misc/pen.py
@@ -304,7 +304,7 @@ class Material:
     	bm=self.getbarmass(mat)
     	rd,speed,mass,en=self.getroundparam(mat)
     	lm=mat.getlm(mass,rd)
-    	bf=self.getbarrelmass(mat,lm,rd,speed)
+    	#bf=self.getbarrelmass(mat,lm,rd,speed)
     	m1=mass
     	if(bm<barmass):
     		return rd,speed,mass,en
