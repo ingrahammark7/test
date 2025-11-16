32e92fdf5ee8672f5fb8ee5c8fb4c9706ef06076
ingrahammark7 <ingrahammark7@gmail.com>
2025-11-16 14:27:55 -0800

    Add files via upload

--- tools/Misc/gw.py
+++ tools/Misc/gw.py
@@ -1,35 +1,117 @@
-import math
-
-# -------------------------------
-# Parameters (J/kg convention)
-# -------------------------------
-U_tensile = 50e6      # J/kg, tensile energy per mass
-E_mass = 10e9          # J/kg, stiffness per mass
-rho_stalk = 200        # kg/m^3, stalk density
-D = 0.03               # m, fixed diameter
-rho_air = 1.2          # kg/m^3
-C_d = 1.2
-v_wind = 4             # m/s
-N_gusts = 1e6          # number of effective gusts per year
-
-# -------------------------------
-# Wind factor
-# -------------------------------
-k = 0.5 * rho_air * C_d * v_wind**2
-
-# -------------------------------
-# Maximum height for fixed D (density included)
-# -------------------------------
-L_max = ((3 * math.pi**2 * rho_stalk * E_mass * U_tensile * D**6) / (128 * k**2 * N_gusts))**0.25
-
-# -------------------------------
-# L/D ratio
-# -------------------------------
-L_D_ratio = L_max / D
-
-# -------------------------------
-# Results
-# -------------------------------
-print(f"Stalk diameter D: {D:.3f} m")
-print(f"Maximum height L_max: {L_max:.2f} m")
-print(f"L/D ratio: {L_D_ratio:.2f}")
+[
+  {
+    "name": "Jerrold Abeles",
+    "dept": "052",
+    "phone": "(213) 633-0652"
+  },
+  {
+    "name": "Kevin C. Brazile",
+    "dept": "020",
+    "phone": "(213) 633-0520"
+  },
+  {
+    "name": "Robert B. Broadbelt",
+    "dept": "053",
+    "phone": "(213) 633-0653"
+  },
+  {
+    "name": "Rupert A. Byrdsong",
+    "dept": "028",
+    "phone": "(213) 633-0528"
+  },
+  {
+    "name": "James C. Chalfant",
+    "dept": "085",
+    "phone": "(213) 830-0785"
+  },
+  {
+    "name": "Wendy Chang",
+    "dept": "036",
+    "phone": "(213) 633-0156"
+  },
+  {
+    "name": "Thomas D. Long",
+    "dept": "048",
+    "phone": "(213) 633-0648"
+  },
+  {
+    "name": "Michael Shultz",
+    "dept": "040",
+    "phone": "(213) 633-0160"
+  },
+  {
+    "name": "Curtis A. Kin",
+    "dept": "086",
+    "phone": "(213) 830-0786"
+  },
+  {
+    "name": "Colin Leis",
+    "dept": "074",
+    "phone": "(213) 830-0774"
+  },
+  {
+    "name": "Maurice A. Leiter",
+    "dept": "054",
+    "phone": "(213) 633-0654"
+  },
+  {
+    "name": "Joseph Lipner",
+    "dept": "072",
+    "phone": "(213) 830-0772"
+  },
+  {
+    "name": "Lia Martin",
+    "dept": "003",
+    "phone": "(213) 633-0253"
+  },
+  {
+    "name": "Alison Mackenzie",
+    "dept": "055",
+    "phone": "(213) 633-0655"
+  },
+  {
+    "name": "Brock T. Hammond",
+    "dept": "031",
+    "phone": "(213) 633-0151"
+  },
+  {
+    "name": "Randolph M. Hammock",
+    "dept": "049",
+    "phone": "(213) 633-0649"
+  },
+  {
+    "name": "Faye C. Barnouw",
+    "dept": "050",
+    "phone": "(213) 633-0650"
+  },
+  {
+    "name": "Gail Killefer",
+    "dept": "037",
+    "phone": "(213) 633-0157"
+  },
+  {
+    "name": "Duffy‑Lewis, Maureen",
+    "dept": "038",
+    "phone": "(213) 633-0158"
+  },
+  {
+    "name": "Fahey, William",
+    "dept": "069",
+    "phone": "(213) 633-1069"
+  },
+  {
+    "name": "Kwan, Ruth Ann",
+    "dept": "089",
+    "phone": "(213) 633-0689"
+  },
+  {
+    "name": "Murillo, Tiana J.",
+    "dept": "078",
+    "phone": "(213) 830-0878"
+  },
+  {
+    "name": "Murphy, Daniel S.",
+    "dept": "032",
+    "phone": "(213) 633-0152"
+  }
+]
