29aca5d0846bde21b409ed4f82534f3d4930566e
ingrahammark7 <ingrahammark7@gmail.com>
2025-11-19 11:28:08 -0800

    Add files via upload

--- tools/Misc/not.py
+++ tools/Misc/not.py
@@ -4,7 +4,7 @@ from reportlab.lib.units import inch
 from reportlab.lib.styles import getSampleStyleSheet
 from reportlab.platypus import Paragraph, Frame
 
-footer_text = "Motion to Refer Transcript to Law Enforcement for Investigation so Judge Can Be Arrested"
+footer_text = "Notice to Court" 
 
 def create_pdf(output_filename):
     # Create a PDF document
@@ -28,13 +28,10 @@ def create_pdf(output_filename):
 
     # Add footer
     def add_footer(page_num):
-        if(page_num==1):
-        	page_num=""
         footer_y = 0.4 * inch  # Position for footer
          # Title of the paper
         c.setFont("Times-Roman", 10)  # Font size for footer
         c.drawCentredString(width / 2, footer_y, footer_text)
-        c.drawString(right_margin-0.05*inch,footer_y-.0*inch, str(page_num))
         # Draw a line above the footer
         c.line(left_margin, footer_y + 0.1 * inch, right_margin, footer_y + 0.1 * inch)
 
@@ -70,11 +67,9 @@ def create_pdf(output_filename):
 
     # (3) Court Title
     c.setFont("Times-Bold", 14)
-    #c.drawCentredString(width / 2, top_margin - 3.33 * inch, "UNITED STATES DISTRICT COURT")
-    #c.drawCentredString(width / 2, top_margin - 3.66 * inch, "CENTRAL DISTRICT OF CALIFORNIA")
     c.drawCentredString(width / 2, top_margin - 3.33 * inch, "SUPERIOR COURT OF CALIFORNIA")
-    c.drawCentredString(width / 2, top_margin - 3.66 * inch, "COUNTY OF LOS ANGELES")
- 
+    c.drawCentredString(width / 2, top_margin - 3.66 * inch, "COUNTY OF SAN FRANCISCO")
+   
 
     # (4) Case Title
     c.setFont("Times-Bold", 12)
@@ -82,7 +77,7 @@ def create_pdf(output_filename):
         "Mark Ingraham,",
         "Plaintiff and Petitioner,",
         "vs",
-        "Los Angeles Police Department",
+        "Chase Bank",
         "Respondent and Defendant.",        
     ]
     y_position = top_margin - 4.75* inch
@@ -98,28 +93,31 @@ def create_pdf(output_filename):
     # (5) Case Number
     c.setFont("Times-Roman", 12)
     space=width / 2 + 1 * inch
-    c.drawString(space, top_margin - 4* inch, "Case Number: 25STCP03825")
+    c.drawString(space, top_margin - 4* inch, "Case Number: CGC-25-625577")
     # (6) Nature of the Paper (moved slightly lower)
     c.setFont("Times-Bold", 12)
-#    c.drawString(space, top_margin - 4.25* inch, "CRS ID: Court did not provide CRS receipt")
+    c.drawString(space, top_margin - 4.25* inch, "Notice to Court")
     c.setFont("Times-Bold", 12)
-    c.drawString(space, top_margin - 4.25* inch, "Hearing Date: 2/3/2026 1:30PM")
-    c.drawString(space,top_margin-4.5*inch, "Hearing ID: Reserved with Court")
-    c.drawString(space,top_margin-4.75*inch,"Motion to Refer Transcript to Law")
-    c.drawString(space,top_margin-5*inch,"Enforcement for Investigation")
-    c.drawString(space,top_margin-5.25*inch,"so Judge Can Be Arrested")
+    c.drawString(space, top_margin - 4.5* inch, "Writ of Mandate")
 
     # (7) Judge and Department
     c.setFont("Times-Roman", 12)
-    c.drawString(space, top_margin - 5.5 * inch, "Judge: ")
-    c.drawString(space, top_margin - 5.75* inch, "Department: 86")
+    c.drawString(space, top_margin - 4.75 * inch, "Judge: Not assigned")
+    c.drawString(space, top_margin - 5* inch, "Department: Not assigned")
 
-    def doer(intro_text,y_position):
-    	return doer1(intro_text,y_position,"Times-Roman",80)
+   
+
+
+    # I. INTRODUCTION
+    c.setFont("Times-Bold", 12)
+    c.drawString(left_margin, y_position - 0.25* inch, footer_text.upper())
+    c.setFont("Times-Roman",12)
+    intro_text = "I ordered the case dismissed sincd judge committed fraud. I included proof of service for the judge's. My filings have been deleted and there is no point in proceeding with case. /n" 
+    y_position -= .25*inch
     
-    def doer1(intro_text,y_position,font,marg):
-    	siz=12
-    	c.setFont(font, siz)
+    
+    def doer(intro_text,y_position):
+    	c.setFont("Times-Roman", 12)
     	spr=intro_text.split(" ")
     	intro_text1=[""]
     	cc=0
@@ -128,36 +126,20 @@ def create_pdf(output_filename):
     		ll=len(s)
     		cc+=ll
     		if(s=="/n"):
-    		    		cc=marg+1
+    		    		cc=81
     		    		s=""
-    		if(cc>marg):
+    		if(cc>80):
     		    		cc=0
     		    		intro_text1.append(tt)
     		    		tt=""
     		tt+=" "
     		tt+=s
     	for line in intro_text1:
-    		if "I." in line or "V." in line:
-    			c.setFont("Times-Bold",siz)
-    		else:
-    			c.setFont(font,siz)
     		c.drawString(left_margin, y_position, line)
     		y_position -= 0.25* inch
     	return y_position
-    	  
-
-
-    # I. INTRODUCTION
-    c.setFont("Times-Bold", 12)
-    y_position=doer1(footer_text.upper() + " /n ",y_position, "Times-Bold",60)
-  #  c.drawString(left_margin, y_position - 0.25* inch, footer_text.upper())
-    c.setFont("Times-Roman",12)
-    intro_text = "I. Introduction /n I request that the Court transmit the transcript and related filings from Ingraham vs LAPD to the appropriate law enforcement authority for review. This request arises from procedural actions and inactions that may constitute obstruction of justice or related misconduct under California law. /n II. Factual Background /n 1. On 10/20 in case 25STCP03728, I filed a request that Judge Kin be arrested for faking transcript in 742 case.  /n 2. Despite proper filing and notice, the Court failed to rule on this motion within the statutory or reasonable period. /n 3. As a result of the Court’s inaction, judge delayed case. /n 4. The inaction appears to have affected matters related to potential criminal violations, including judge faking the transcript in the case. /n III. Legal Basis /n Obstruction of justice – Penal Code §§ 136–137 /n Conspiracy to obstruct justice – Penal Code § 182(a)(5) /n Falsification or concealment of court records – Penal Code § 115 /n The Court has authority to refer records or transcripts to law enforcement when procedural irregularities indicate potential criminal activity. (See CCP § 2025(f); Cal. Rules of Court, Rule 8.155(c)) /n IV. Relief Requested /n I request that the Court: /n 1. Transmit the transcript and all related filings from Ingraham vs LAPD to the US Attorney for the Central District of California and any other appropriate law enforcement agencies. /n 2. If the name of this motion is changed, clerk is guilty of fraud for changing the title. /n " 
-    y_position -= .25*inch
-    
-    
-    
-  
+    	
+    y_position=doer(intro_text,y_position)
     
     
     
@@ -168,11 +150,9 @@ def create_pdf(output_filename):
     	add_footer(num)
     	cc.showPage()
     	return cc
-    c=dopage(c,1)
-    y_position=top_margin-.25*inch
-    y_position=doer1(intro_text,y_position,"Times-Roman",80) 	
+    	
     c.setFont("Times-Roman", 12)
-    c.drawString(left_margin, y_position - 0.25* inch, "Submitted, Mark Ingraham, 10/30/2025")
+    c.drawString(left_margin, y_position - 0.25* inch, "Submitted, Mark Ingraham, 11/19/2025")
     c=dopage(c,2)
 
 
