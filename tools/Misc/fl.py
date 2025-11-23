95c5240ce5a24b1e4bd08aae50300938d90917ea
ingrahammark7 <ingrahammark7@gmail.com>
2025-11-22 16:11:18 -0800

    Add files via upload

---
+++ tools/Misc/fl.py
@@ -0,0 +1,16 @@
+from flask import Flask, send_from_directory
+
+app = Flask(__name__, static_folder='static')
+
+# Serve the HTML page
+@app.route('/')
+def index():
+    return send_from_directory('static', 'terrain.html')
+
+# Serve JSON files automatically
+@app.route('/<path:filename>')
+def serve_files(filename):
+    return send_from_directory('static', filename)
+
+if __name__ == '__main__':
+    app.run(debug=True)
