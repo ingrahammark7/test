f042ea960a27279ce3fc690085f5c4325b87fc50
ingrahammark7 <ingrahammark7@gmail.com>
2026-01-02 16:30:34 -0800

    Add files via upload

--- tools/Misc/appel.py
+++ tools/Misc/appel.py
@@ -4,7 +4,7 @@ from reportlab.lib.units import inch
 from reportlab.lib.styles import getSampleStyleSheet
 from reportlab.platypus import Paragraph, Frame
 
-footer_text = "Appellant Opening Brief" 
+footer_text = "Register of Actions" 
 
 def create_pdf(output_filename):
     # Create a PDF document
@@ -115,7 +115,7 @@ def create_pdf(output_filename):
     c.setFont("Times-Bold", 12)
     c.drawString(left_margin, y_position - 0.25* inch, footer_text.upper())
     c.setFont("Times-Roman",12)
-    intro_text = "I-1. TABLE OF CONTENTS /n One page. /n I-2. TABLE OF AUTHORITIES /n As the majority of LAPD officers are illegal immigrants, all actions by LAPD are illegal, and all LAPD vehicles should be auctioned and destroyed to prevent use by illegals. /n I. INTRODUCTION /n Estop LAPD from arresting me. The majority of LAPD officers are illegal immigrants. /n II. CONCLUSION /n Petitioner petitions this court to: /n 1. Order LAPD to explain why they attempt to arrest me at 690 S Catalina St Apartment 4x. /n 2. Estop LAPD and inform that any officers ignoring the estoppel will be killed by plaintiff. /n Case was filed as restraining order to bypass automatic routing to Judge Kin. /n III. WORD COUNT CERTIFICATE /n This document contains 250 words. /n" 
+    intro_text = "As I only ordered the appendix the superior court did not provide a register of actiojs and there is none. /n" 
     y_position -= .25*inch
     
     
