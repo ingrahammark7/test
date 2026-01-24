3ad4763af09b2631b1437ce016281a73f1f0dd1e
ingrahammark7 <ingrahammark7@gmail.com>
2026-01-24 15:55:14 -0800

    Add files via upload

--- tools/Misc/gw.py
+++ tools/Misc/gw.py
@@ -1,25 +1,35 @@
-n=1e30
-r=1e-12
-c = 299792458.0
-G = 6.67430e-11
-h = 6.62607015e-34
-m_e = 9.10938356e-31
-ev=1e-19
-vol=r**3
-dens=n*ev/vol
-m=ev/c**2
-gr=G*m*m
-gr/=r
-ac=2*gr/m
-ac**=.5
-acc=G*(2*m)/r**2
-vo=G*(2*m*n)/c
-vo**=.5
-minr=vo
-volm=minr**3
-vols=vol/volm
-volt=c/minr
-tt=vols*volt
-n2=n**2
-tt/=n2
-print(tt,"seconds to crirical")
+# Constants
+c = 299792458.0          # speed of light (m/s)
+G = 6.67430e-11          # gravitational constant (m^3 kg^-1 s^-2)
+
+# Inputs
+n = 1e60                # number of photons
+E_gamma = 1e-19         # energy per photon (J)
+r = 1e-12               # radius of confinement (m)
+
+# Total energy and mass
+E_total = n * E_gamma
+M = E_total / c**2
+
+# Schwarzschild radius
+r_s = 2 * G * M / c**2
+
+# Energy density
+volume = (4/3) * 3.141592653589793 * r**3
+rho_E = E_total / volume
+
+# Light-crossing time
+t_light = r / c
+
+# Output
+print("Total energy (J):", E_total)
+print("Equivalent mass (kg):", M)
+print("Schwarzschild radius (m):", r_s)
+print("System radius (m):", r)
+print("Energy density (J/m^3):", rho_E)
+print("Light-crossing time (s):", t_light)
+
+if r <= r_s:
+    print(">>> Collapse inevitable: trapped surface forms")
+else:
+    print(">>> No horizon yet (but increasing density strengthens gravity)")
