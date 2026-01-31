337678850bdb2cd628d6398d4fb123dcd873537c
ingrahammark7 <ingrahammark7@gmail.com>
2026-01-31 15:06:28 -0800

    Add files via upload

--- tools/Misc/gw.py
+++ tools/Misc/gw.py
@@ -1,15 +1,13 @@
 # Parameters
 hh = 0.0127         # object thickness in meters
 hl = 511e3          # reference energy in eV
-step = 10000        # scaling step
 ro = 2.5e-12        # scaling constant for HVL
 
-# Search loop
-for i in range(1000):
-    fl = step * i * i + 1
-    re = hl / fl      # energy in eV
-    ra = ro * fl      # scaled HVL
-    h1 = hh / fl
-    if h1 < ra:
-        print("Crossover energy (eV):", re)
-        break
+# Direct calculation of crossover
+# h1 / fl = ra -> hh / fl = ro * fl -> fl^2 = hh / ro -> fl = sqrt(hh / ro)
+fl_crossover = (hh / ro) ** 0.5
+
+# Energy at crossover
+crossover_energy = hl / fl_crossover
+
+print("Crossover energy (eV):", crossover_energy)
