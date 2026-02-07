7b82e615f8e8040c4b19e71017c9c7612e869497
ingrahammark7 <ingrahammark7@gmail.com>
2026-02-07 10:52:25 -0800

    Add files via upload

--- tools/Misc/not.py
+++ tools/Misc/not.py
@@ -4,7 +4,7 @@ from reportlab.lib.units import inch
 from reportlab.lib.styles import getSampleStyleSheet
 from reportlab.platypus import Paragraph, Frame
 
-footer_text = "Affidavit of Service"
+footer_text = "Request to Dismiss Case"
 
 def create_pdf(output_filename):
     # Create a PDF document
@@ -77,7 +77,7 @@ def create_pdf(output_filename):
         "Mark Ingraham,",
         "Plaintiff and Petitioner,",
         "vs",
-        "Los Angeles City Attorney",
+        "Los Angeles Police Department",
         "Respondent and Defendant.",        
     ]
     y_position = top_margin - 4.75* inch
@@ -93,7 +93,7 @@ def create_pdf(output_filename):
     # (5) Case Number
     c.setFont("Times-Roman", 12)
     space=width / 2 + 1 * inch
-    c.drawString(space, top_margin - 4* inch, "Case Number: 26STCP00175")
+    c.drawString(space, top_margin - 4* inch, "Case Number: 25STCP03825")
     # (6) Nature of the Paper (moved slightly lower)
     c.setFont("Times-Bold", 12)
     c.drawString(space, top_margin - 4.25* inch, footer_text)
@@ -112,7 +112,7 @@ def create_pdf(output_filename):
     c.setFont("Times-Bold", 12)
     c.drawString(left_margin, y_position - 0.25* inch, footer_text.upper())
     c.setFont("Times-Roman",12)
-    intro_text = "Attached packet was submitted with $50 today at room 525 court services desk. /n /n /n" 
+    intro_text = "Clerk should be fired for repeatedly assigning case to Curtis Kin. Kin wrote a decision in 25STCP03728 ordering himself arrested and is disqualfied from all of my cases. There is no point in continuing with case and all LASC judges are in contempt for ignoring disqualification and failing to transmit appeals, and all judges should be arrested. /n" 
     y_position -= .25*inch
     
     
