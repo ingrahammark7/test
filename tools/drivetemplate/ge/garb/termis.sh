bd9b8aedc8b205ac150c449fba47dbc72ee69ef0
ingrahammark7 <ingrahammark7@gmail.com>
2026-02-13 03:25:14 -0800

    Update pip packages in termis.sh script

--- tools/drivetemplate/ge/garb/termis.sh
+++ tools/drivetemplate/ge/garb/termis.sh
@@ -3,7 +3,7 @@ pkg update -y
 termux-setup-storage
 pkg install -y bash
 pkg install -y freetype libjpeg-turbo libpng jq pandoc enscript texlive iconv mupdf-tools pdftk qpdf emacs wget libxml2 geckodriver wget wget2 e2fsprogs python python-pip nano ffmpeg git make lsof gzip clang cmake htop bat fd ripgrep tig asciinema neofetch  nnn aria2 mc lftp fzf pacman termux-api pdftk imagemagick libffi freetype pango cmatrix sl figlet toilet cowsay fortune ninvaders moon-buggy greed rclone procps util-linux coreutils pv
-pip install pikepdf anywidget yfinance tensorflow tqdm noise plyer astunparse plotly jupyter convertdate flask_session pathlib lolcat kivymd pdf2image pymupdf yt-dlp setuptools youtube-dl gallery-dl beautifulsoup4 build reportlab sympy pypdf2 poppler ghostscript mps-youtube black pyautogui pynput pywinauto pygetwindow uiautomator kivy kivy3 numpy-stl pyopengl pyopengl_accelerate ffmpeg ipywidgets
+pip install geomdl pyqt5 trimesh pyvista pikepdf anywidget yfinance tensorflow tqdm noise plyer astunparse plotly jupyter convertdate flask_session pathlib lolcat kivymd pdf2image pymupdf yt-dlp setuptools youtube-dl gallery-dl beautifulsoup4 build reportlab sympy pypdf2 poppler ghostscript mps-youtube black pyautogui pynput pywinauto pygetwindow uiautomator kivy kivy3 numpy-stl pyopengl pyopengl_accelerate ffmpeg ipywidgets
 apt install root-repo -y
 apt install iw -y
 apt install libc++ libnl libpcap libsqlite openssl pcre zlib -y
