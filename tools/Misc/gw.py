710a94034c45c7f970da2c318eb9761cdc361e10
ingrahammark7 <ingrahammark7@gmail.com>
2026-04-13 10:48:28 -0700

    Add files via upload

--- tools/Misc/gw.py
+++ tools/Misc/gw.py
@@ -1,29 +1,14 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
-# Parameter space
-Kn = np.logspace(-2, 2, 200)
-eps = np.logspace(-2, 2, 200)
-
-K, E = np.meshgrid(Kn, eps)
-
-# Regime classification (heuristic)
-Z = np.zeros_like(K)
-
-# 0 = diffusive
-Z[(K < 1) & (E < 1)] = 0
-
-# 1 = transitional
-Z[(np.abs(np.log10(K)) <= 0.5) | (np.abs(np.log10(E)) <= 0.5)] = 1
-
-# 2 = ballistic / sputter-dominant
-Z[(K > 1) & (E > 1)] = 2
-
-plt.figure()
-plt.contourf(np.log10(K), np.log10(E), Z)
-
-plt.xlabel("log10(Kn = λ/L)")
-plt.ylabel("log10(ε = ion energy / threshold)")
-plt.title("Unified Kn–ε Phase Diagram (η implicit)")
-
-plt.show()
+h1bbase=85000
+nonwage=h1bbase*1.5
+stock=nonwage*4
+contractor=stock*3
+revenue={"google":[400e9,200e3,.32],"meta":[200e9,75e3,.34],"apple":[420e9,170e3,.27],"amazon":[716e9,400e3,.1],"nflx":[45e9,16e3,.24]}
+for i in revenue:
+	re=revenue[i]
+	rme=re[0]
+	em=re[1]
+	cost=contractor*em
+	rep=cost/rme
+	pro=1-rep
+	pp=re[2]
+	print(i,"estimated profit ",pro, "actual",pp)
