7e4c37f5f62f8450e830a2d92df491673925caef
ingrahammark7 <ingrahammark7@gmail.com>
2026-04-22 21:16:38 -0700

    Add files via upload

--- tools/Misc/gw.py
+++ tools/Misc/gw.py
@@ -1,40 +1,21 @@
 import numpy as np
 
-# -----------------------------
-# Parameters (with implicit units)
-# -----------------------------
-Phi_B = 1e18   # atoms / m^2 / s
-Phi_0 = 2e18   # atoms / m^2 / s
-ns    = 1e19   # atoms / m^2
-v0    = 1.0    # m / s
-alpha = 1.0    # dimensionless
-t     = 1e-9   # s
-y     = 0.5    # normalized depth [0,1]
-thermalvelocity=500#m/s
-atomradius=.2e-9#m
-etchrate=4e-6 #m/s
-etchrate/=atomradius
-therm=thermalvelocity/atomradius
+# physical parameters
+Phi = 2e18      # atoms / m^2 / s
+ns  = 1e19      # atoms / m^2
+v_etch = 4e-6   # m / s
 
-# -----------------------------
-# Dimensionless exposure
-# -----------------------------
-H_B = (Phi_B * t) / ns
-H_I = (Phi_0 * (1 + alpha * y) * t) / ns
-ratio=therm/etchrate
-pow=ratio**(1/3)
-print("number of atoms hit before slowing to etch rate",pow)
-print("size nanometers",pow*atomradius*1e9)
-# -----------------------------
-# Velocity law
-# -----------------------------
-v_B = v0 / (1 + H_B**3)
-v_I = v0 / (1 + H_I**3)
+# reaction rate per site
+r = Phi / ns  # 1/s
 
+# Damköhler length scale (correct form)
+ell_star = v_etch / r
 
-# -----------------------------
-# Output
-# -----------------------------
-print("Brownian velocity:", v_B, "m/s")
-print("Ion-directed velocity:", v_I, "m/s")
-print("Ion > Brown:", v_I > v_B)
+print("Damköhler length scale (m):", ell_star)
+print("Damköhler length scale (nm):", ell_star * 1e9)
+
+# Damköhler number for reference length (1 nm)
+l_ref = 1e-9
+Da = (r * l_ref) / v_etch
+
+print("Damköhler number at 1 nm:", Da)
