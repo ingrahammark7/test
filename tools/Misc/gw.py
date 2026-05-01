2aecd1db60040e3787d4be0c4a28b63a6161eb81
ingrahammark7 <ingrahammark7@gmail.com>
2026-05-01 11:26:20 -0700

    Add files via upload

--- tools/Misc/gw.py
+++ tools/Misc/gw.py
@@ -13,5 +13,26 @@ ev=1.6e-19
 r=apm/ev
 print(r,"ev rate per s")
 r=hc/apm
-print(r,"latency")
-print(r*1e9,"nanoseconds")
+print(r*1e6,"microseconds")
+rr=r
+spe=1/(c/ald)
+fo=rr/spe
+alm=5e-9
+evm=ev/(c**2)
+bm=1.38e-23
+almm=4.5e-26
+tt=273
+vt=((3*bm*tt)/almm)**.5
+tik=alm/vt
+ue=(ev*tik)/evm
+print(ue,"ideal electron mobility m2/vs")
+avta=.01
+print("actual aluminum mobikity",avta)
+cro=c/ald
+all=(ald*ald)*cro
+ue/=all
+avta/=all
+phi=(1+5**.5)/2
+print(ue,avta,"in units of atom crossing rate")
+ue**=(1/3)
+print(ue/138/phi,"normed")
