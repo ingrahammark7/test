7d00206ffa0902e682470792a20d433c07133b94
ingrahammark7 <ingrahammark7@gmail.com>
2026-05-03 15:06:41 -0700

    Add files via upload

--- tools/Misc/not.py
+++ tools/Misc/not.py
@@ -4,7 +4,7 @@ from reportlab.lib.units import inch
 from reportlab.lib.styles import getSampleStyleSheet
 from reportlab.platypus import Paragraph, Frame
 
-footer_text = "Notice of Appeal"
+footer_text = "Motion to Dismiss"
  
 def create_pdf(output_filename):
     # Create a PDF document
@@ -77,7 +77,7 @@ def create_pdf(output_filename):
         "Mark Ingraham,",
         "Plaintiff and Petitioner,",
         "vs",
-        "Los Angeles Police Department",
+        "California",
         "Respondent and Defendant.",        
     ]
     y_position = top_margin - 4.75* inch
@@ -93,12 +93,12 @@ def create_pdf(output_filename):
     # (5) Case Number
     c.setFont("Times-Roman", 12)
     space=width / 2 + 1 * inch
-    c.drawString(space, top_margin - 4* inch, "Case Number: 25STCP03825")
+    c.drawString(space, top_margin - 4* inch, "Case Number: 25ARCM01736-01")
     # (6) Nature of the Paper (moved slightly lower)
     c.setFont("Times-Bold", 12)
     c.drawString(space, top_margin - 4.25* inch, footer_text)
     c.setFont("Times-Bold", 12)
-    c.drawString(space, top_margin - 4.5* inch, "Writ of Mandate")
+    c.drawString(space, top_margin - 4.5* inch, "Petition for Writ of Mandate")
 
     # (7) Judge and Department
     c.setFont("Times-Roman", 12)
@@ -112,7 +112,7 @@ def create_pdf(output_filename):
     c.setFont("Times-Bold", 12)
     c.drawString(left_margin, y_position - 0.25* inch, footer_text.upper())
     c.setFont("Times-Roman",12)
-    intro_text = "Notice is given hearing outcome on 2/26/2026 is appealed. Judge did not file anything but made statements indicating case is dismissed. Record request APP-003 is also filed. /n" 
+    intro_text = "The majority of SMPD officers are illegal immigrants. Therefore, SMPD is guilty of treason and should be killed. Petitioner petitions Court to: /n 1. Inform all SMPD officers they will be killed by me. /n 2. Order all persons to kill everyone associated with SMPD, all courts, and anything else. /n" 
     y_position -= .25*inch
     
     
